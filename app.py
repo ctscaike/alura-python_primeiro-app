09d427e091fc9033ed6fd39e6b3cbcdb59c62f1b
Caike Thiago Santos <ctscaike@gmail.com>
2025-08-26 14:55:50 -0300

    Primeiras aulas de python na Alura

---
+++ app.py
@@ -0,0 +1,71 @@
+import os;
+
+restaurantes = ['Pizza','Churras'];
+
+def exibir_nome_do_programa():    
+    print ('Sabor Express\n');
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante');
+    print('2. Listar restaurante');
+    print('3. Ativar restaurante');
+    print('4. Sair\n');
+
+def finalizar_app() :
+    os.system('cls');
+    print('Encerrando o APP\n');
+
+def opcao_invalida():
+    print('Opção Inválida\n');
+    input('Digite uma tecla para voltar ao menu principal: ');
+    main();
+
+def cadastrar_novo_restaurante():
+    os.system('cls');
+    print('Cadastro de novos restaurantes\n');
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ');
+    restaurantes.append(nome_do_restaurante);
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n');
+    input('\nDigite uma tecla para voltar ao menu principal: ');
+    main();
+
+def listar_restaurantes():
+    os.system('cls');
+    print('Listando restaurantes\n');
+
+    for restaurante in restaurantes:
+        print(f'. {restaurante}');
+    input('\nDigite uma tecla para voltar ao menu principal: ');
+    main();
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '));
+        print(f'Você escolheu a opção: {opcao_escolhida}');
+
+        if opcao_escolhida == 1 :
+            #print('Cadastrar restaurante');
+            cadastrar_novo_restaurante();
+        elif opcao_escolhida == 2 :
+            #print('Listar restaurante');
+            listar_restaurantes();
+        elif opcao_escolhida == 3 :
+            print('Ativar restaurante');
+        elif opcao_escolhida == 4 :
+            finalizar_app();
+        else :
+        #    print('Encerrando o programa');
+        #    finalizar_app();
+            opcao_invalida();
+    except:
+        opcao_invalida();
+
+def main():
+    os.system('cls');
+    exibir_nome_do_programa();
+    exibir_opcoes();
+    escolher_opcao();
+
+if __name__ == '__main__':
+    main();
+    
