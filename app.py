d535003367bc767f549a4b5d331b51dd632bf917
Caike Sannto <ctscaike@gmail.com>
2025-08-26 17:24:29 -0300

    Adicionar função ativar_restaurante

--- app.py
+++ app.py
@@ -49,6 +49,23 @@ def listar_restaurantes():
         print(f'- {nome_restaurante} | {categoria} | {ativo}');
     voltar_ao_menu_principal();
 
+def alternar_estado_do_restaurante():
+    exibir_subtitulo('Alternanto o estado do restaurante');
+    nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+    restaurante_encontrado = False;
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True;
+            restaurante['ativo'] = not restaurante['ativo'];
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso\n' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso';
+            print(mensagem);
+    
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado');
+    
+    voltar_ao_menu_principal();
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '));
@@ -61,7 +78,8 @@ def escolher_opcao():
             #print('Listar restaurante');
             listar_restaurantes();
         elif opcao_escolhida == 3 :
-            print('Ativar restaurante');
+            #print('Ativar restaurante');
+            alternar_estado_do_restaurante();
         elif opcao_escolhida == 4 :
             finalizar_app();
         else :
